9496c9d95141211b809abcd41317b1fb031384f3
André Büsgen <andre.buesgen@posteo.de>
2024-02-19 10:07:00 +0100

    feat: :sparkles: initial import

---
+++ rockbox_listenbrainz_scrobbler/cli.py
@@ -0,0 +1,76 @@
+import csv
+from pathlib import Path
+
+import pylistenbrainz
+import typer
+from typing_extensions import Annotated
+
+from rockbox_listenbrainz_scrobbler.model import ScrobblerEntry
+
+app = typer.Typer()
+
+
+@app.command()
+def upload_rockbox(
+    rockbox_scrobbler_log_path: Path,
+    auth_token: Annotated[
+        str,
+        typer.Option(
+            prompt="Please provide your listenbrainz Token (input hidden)",
+            hide_input=True,
+        ),
+    ],
+):
+    scrobbles = []
+    with rockbox_scrobbler_log_path.open(
+        "r",
+        encoding="utf8",
+        newline="",
+    ) as fp_scrobbler:
+        for _ in range(4):  # skip header
+            fp_scrobbler.readline()
+        reader = csv.DictReader(
+            fp_scrobbler,
+            delimiter="\t",
+            fieldnames=[
+                "#ARTIST",
+                "#ALBUM",
+                "#TITLE",
+                "#TRACKNUM",
+                "#LENGTH",
+                "#RATING",
+                "#TIMESTAMP",
+                "#MUSICBRAINZ_TRACKID",
+            ],
+        )
+        for row in reader:
+            scrobbles += [ScrobblerEntry(**row)]
+
+    listens = [
+        pylistenbrainz.Listen(
+            track_name=listen.title,
+            artist_name=listen.artist,
+            release_name=listen.album,
+            listened_at=listen.timestamp,
+        )
+        for listen in scrobbles
+    ]
+
+    client = pylistenbrainz.ListenBrainz()
+    client.set_auth_token(auth_token)
+
+    client.submit_multiple_listens(listens)
+
+
+def main():
+    """
+    function entrypoint for tools.poetry.scripts
+    """
+    app()
+
+
+if __name__ == "__main__":
+    """
+    entrypoint for running this script using python
+    """
+    main()
---
+++ rockbox_listenbrainz_scrobbler/model.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import AliasChoices, BaseModel, Field
+
+
+class ScrobblerEntry(BaseModel):
+    artist: str = Field(validation_alias=AliasChoices("artist", "#ARTIST"))
+    album: str = Field(validation_alias=AliasChoices("album", "#ALBUM"))
+    title: str = Field(validation_alias=AliasChoices("title", "#TITLE"))
+    tracknum: int = Field(validation_alias=AliasChoices("tracknum", "#TRACKNUM"))
+    length: int = Field(validation_alias=AliasChoices("length", "#LENGTH"))
+    rating: str = Field(validation_alias=AliasChoices("rating", "#RATING"))
+    timestamp: int = Field(validation_alias=AliasChoices("timestamp", "#TIMESTAMP"))
+    musicbrainz_trackid: str = Field(
+        validation_alias=AliasChoices("musicbrainz_trackid", "#MUSICBRAINZ_TRACKID")
+    )
