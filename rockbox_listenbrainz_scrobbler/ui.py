50abd997cf322aef6da53252e5eebdeeb67bfb2d
André Büsgen <andre.buesgen@posteo.de>
2024-02-23 20:28:20 +0100

    refactor: minor improvements

--- rockbox_listenbrainz_scrobbler/ui.py
+++ rockbox_listenbrainz_scrobbler/ui.py
@@ -24,12 +24,13 @@ from rockbox_listenbrainz_scrobbler.scrobbling import (
     read_rockbox_log,
 )
 
+# Global constants
 LISTENBRAINZ_AUTH_TOKEN_SETTING_KEY: str = "Listenbrainz/auth-token"
 
 
 def show_error(text: str):
     """
-    Show a Error message with the given Text
+    Display an error message with the given text
     """
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
@@ -40,8 +41,13 @@ def show_error(text: str):
 
 
 class ListenbrainzWidget(QWidget):
+    """
+    Main GUI widget for the Rockbox Listenbrainz Scrobbler
+    """
+
     def __init__(self):
         super().__init__()
+
         self.settings = QSettings("AnotherStranger", "Rockbox Listenbrainz Scrobbler")
 
         self.setWindowTitle("Rockbox Listenbrainz Scrobbler")
@@ -49,7 +55,7 @@ class ListenbrainzWidget(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        # Passwortfeld für Auth-Token
+        # Input field for auth token
         auth_layout = QHBoxLayout()
         layout.addLayout(auth_layout)
 
@@ -58,6 +64,9 @@ class ListenbrainzWidget(QWidget):
 
         self.auth_token_input = QLineEdit(self)
         self.auth_token_input.setPlaceholderText("User Token")
+        self.auth_token_input.setToolTip(
+            "Your personal auth token for ListenBrainz. Get it here: https://listenbrainz.org/settings/"
+        )
         if self.settings.value(LISTENBRAINZ_AUTH_TOKEN_SETTING_KEY):
             self.auth_token_input.setText(
                 self.settings.value(LISTENBRAINZ_AUTH_TOKEN_SETTING_KEY)
@@ -65,41 +74,53 @@ class ListenbrainzWidget(QWidget):
 
         auth_layout.addWidget(self.auth_token_input)
 
-        # Dateiauswahl-Button
+        # Button for selecting log file
         self.file_button = QPushButton("Select Logfile...", self)
         self.file_button.clicked.connect(self.select_file)
 
-        # Submit-Button
+        # Submit button
         self.submit_button = QPushButton("Submit", self)
         self.submit_button.clicked.connect(self.submit_data)
         self.submit_button.setEnabled(False)
 
-        # Horizontales Layout für beide Buttons
+        # Horizontal layout for both buttons
         buttons_layout = QHBoxLayout()
         buttons_layout.addWidget(self.file_button)
         buttons_layout.addWidget(self.submit_button)
         layout.addLayout(buttons_layout)
 
-        # Fortschrittsbalken
+        # Progress bar
         self.progress_bar = QProgressBar(self)
         layout.addWidget(self.progress_bar)
 
-        # Textfeld
+        # Read-only text edit area
         self.text_edit = QTextEdit(self)
         self.text_edit.setReadOnly(True)
         layout.addWidget(self.text_edit)
 
     def select_file(self):
+        """
+        Opens a file dialog allowing the user to select a log file and configures the UI accordingly
+        """
         file_dialog = QFileDialog(self)
         file_dialog.setNameFilter("All Files (*);;Rockbox Scrobbler Log (*.log)")
         if file_dialog.exec_():
-            self.selected_file = Path(file_dialog.selectedFiles()[0])
-            self.submit_button.setEnabled(True)
-            self.text_edit.setText(self.selected_file.open("r", encoding="utf8").read())
+            selected_file = Path(file_dialog.selectedFiles()[0])
+            if selected_file.exists():
+                self.selected_file = selected_file
+                self.submit_button.setEnabled(True)
+                content = selected_file.open("r", encoding="utf8").read()
+                self.text_edit.setText(content)
+            else:
+                show_error("Selected file does not exist.")
         else:
             self.submit_button.setEnabled(False)
 
     def submit_data(self):
+        """
+        Retrieves the auth token, saves it in the settings, reads the log file, sends scrobbles to
+        the ListenBrainz server, and updates the UI accordingly
+        """
         auth_token = self.auth_token_input.text()
         if not auth_token:
             show_error(
@@ -111,7 +132,8 @@ class ListenbrainzWidget(QWidget):
 
         try:
             scrobbles = read_rockbox_log(self.selected_file)
-            self.progress_bar.setMaximum(len(scrobbles))
+            total_scrobbles = len(scrobbles)
+            self.progress_bar.setMaximum(total_scrobbles)
             client = ListenBrainzScrobbler(auth_token)
 
             for i, scrobble in enumerate(scrobbles):
